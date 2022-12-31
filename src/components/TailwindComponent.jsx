3ad5afdfe0462c3e2f4213b5255e1ba2f45271b5
ddungpal <ddungpal@naver.com>
2022-12-31 23:20:05 +0900

    최초등록

---
+++ src/components/TailwindComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function TailwindComponent() {
+    return <div>
+        <h1 className="text-8xl">안녕!</h1>
+        <button className="bg-blue-500 rounded-xl px-3">Button</button>
+    </div>;
+}
